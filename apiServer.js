444521cc3fbc00258701fd81e840450e5bb1ffd4
zocalop <jcb2861@gmail.com>
2026-07-05 18:38:29 -0300

    Updated apiServer.js in main

--- apiServer.js
+++ apiServer.js
@@ -2,14 +2,20 @@
 const express = require("express");
 const myapp = express();
 const jsonwebtoken = require("jsonwebtoken");
-const JWT_SECRET = "aVerySecretString");
+const JWT_SECRET = "aVerySecretString";
 
+myapp.use(express.json());
 
-
-myapp.get("/employees", (req, res) => {
+myapp.post("/signin", (req, res) => {
+  const { uname, pwd} = req.body;
+  if (uname === "user" && pwd === "password") {
+    return res.json({
+      token: jsonwebtoken.sign({ user: "user" }, JWT_SECRET),
+    });
+  }
   return res
     .status(401)
-    .json({ message: "Please login to access this resource" });
+    .json({ message: "Invalid username and/or password"});
 });
 
 myapp.listen(5000, () => {
